ddee011bd4d21cd83a654640355c10c433a10039
man90 <man90@tuta.io>
2020-12-05 14:00:21 +0100

    Update app info on the landing page

--- src/components/landing/LandingInfo.jsx
+++ src/components/landing/LandingInfo.jsx
@@ -3,14 +3,36 @@ import styled from 'styled-components'
 
 const Info = styled.div`
 	display: flex;
+	flex-direction: column;
+	user-select: none;
+`
+
+const Header = styled.div`
+	font-size: 3em;
+`
+
+const LastLine = styled.div`
+	margin-top: 1em;
+`
+
+const Link = styled.a`
+	color: #fff;
+	text-decoration: underline;
 `
 
 export default function LandingInfo() {
 	return (
 		<Info>
-			STOS Team Organisation System is an open-source tool
-			<br />
-			for managing your team's work.
+			<Header>STOS</Header>
+			<div>
+				STOS Team Organisation System is an open-source tool
+			</div>
+			<div>
+				for managing your team's workflow.
+			</div>
+			<LastLine>
+				Check out project's <Link href="https://gitlab.com/man90/stos" target="_blank">GitLab page</Link>.
+			</LastLine>
 		</Info>
 	)
 }
