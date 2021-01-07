56e1350ef7fe6c4b20926b362064d72967e054c7
man90 <man90@tuta.io>
2021-01-07 10:46:57 +0100

    Rename dashboard modifiers

--- src/backend/domain/entity/Dashboard.go
+++ src/backend/domain/entity/Dashboard.go
@@ -6,7 +6,7 @@ type Dashboard struct {
 	ID 				primitive.ObjectID 		`bson:"_id,omitempty" json:"id"`
 	Title 			string 					`bson:"title,omitempty" json:"title"`
 	Owner			primitive.ObjectID 		`bson:"owner,omitempty" json:"owner"`
-	Users 			[]primitive.ObjectID 	`bson:"users,omitempty" json:"users,omitempty"`
-	UsersCanEdit 	bool 					`bson:"usersCanEdit,omitempty" json:"usersCanEdit"`
-	EveryoneCanView bool 					`bson:"everyoneCanView,omitempty" json:"everyoneCanView"`
+	Users 			[]primitive.ObjectID 	`bson:"users,omitempty" json:"users"`
+	Collaborative 	bool 					`bson:"collaborative,omitempty" json:"collaborative"`
+	Public 			bool 					`bson:"public,omitempty" json:"public"`
 }
