350aceca5a820068f134924cf4ac348b7e8ba980
man90 <man90@tuta.io>
2021-04-27 13:59:40 +0200

    Improve code style in the task component

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,21 +1,21 @@
-import React 						from 'react'
-import styled, { css } 				from 'styled-components'
-import { Draggable } 				from 'react-beautiful-dnd'
+import React                        from 'react'
+import styled, { css }              from 'styled-components'
+import { Draggable }                from 'react-beautiful-dnd'
 import { useSelector, useDispatch } from 'react-redux'
-import { dispatch as busDispatch } 	from 'use-bus'
-import PlusIcon 					from '@material-ui/icons/Add'
+import { dispatch as busDispatch }  from 'use-bus'
+import PlusIcon                     from '@material-ui/icons/Add'
 
-import Description 	from './taskModules/Description.jsx'
-import Poll			from './taskModules/Poll'
-import Timer 		from './taskModules/Timer.jsx'
-import UserList 	from './taskModules/UserList.jsx'
-import controller	from '../controller'
+import Description from './taskModules/Description.jsx'
+import Poll        from './taskModules/Poll'
+import Timer       from './taskModules/Timer.jsx'
+import UserList    from './taskModules/UserList.jsx'
+import controller  from '../controller'
 
 const modules = {
-	description: 	Description,
-	poll: 			Poll,
-	timer: 			Timer,
-	userList: 		UserList
+	description: Description,
+	poll:        Poll,
+	timer:       Timer,
+	userList:    UserList
 }
 
 const Container = styled.div`
@@ -28,55 +28,67 @@ const Container = styled.div`
 	${props => props.createTaskButton && css`
 		color: #777;
 		align-items: center;
+		cursor: pointer;
 	`}
 `
 
-export default function Task({ taskID, index, listID }) {
-	const tasks = useSelector(state => state.tasks)
+function RealTask({ taskID, index }) {
 	const task = useSelector(state => state.tasks[taskID])
-	const dispatch = useDispatch()
 
-	function taskClickHandler() {
+	function clickHandler() {
 		busDispatch({ type: 'showTaskModal', taskID })
 	}
 
-	function createTaskClickHandler() {
-		let listTasks = Object.values(tasks).filter(task => task.list === listID)
-		let index = listTasks.length ? listTasks.sort((a, b) => b.index - a.index)[0].index + 1 : 0
+	return (
+		<Draggable draggableId={ taskID } index={ index }>
+			{(provided) => (
+				<Container { ...provided.draggableProps } { ...provided.dragHandleProps } ref={ provided.innerRef } onClick={ clickHandler }>
+					{ task.title }
+					{ task.modules &&
+						task.modules.map((moduleJSON) => {
+							const module = JSON.parse(moduleJSON)
+
+							return React.createElement(
+								modules[module.type],
+								{
+									key: module.id,
+									meta: module,
+									full: false,
+									task
+								}
+							)
+						})
+					}
+				</Container>
+			)}
+		</Draggable>
+	)
+}
+
+function TaskPlaceholder({ listID }) {
+	const tasks = useSelector(state => state.tasks)
+	const dispatch = useDispatch()
+
+	function clickHandler() {
+		const listTasks = Object.values(tasks).filter(task => task.list === listID)
+		const index = listTasks.length
+			? listTasks.sort((a, b) => b.index - a.index)[0].index + 1
+			: 0
 
 		controller.createTask(listID, index, dispatch)
 	}
 
-	if (task) {
-		return (
-			<Draggable draggableId={ taskID } index={ index }>
-				{ provided => (
-					<Container { ...provided.draggableProps } { ...provided.dragHandleProps } ref={ provided.innerRef } onClick={ taskClickHandler }>
-						{ task.title }
-						{ task.modules &&
-							task.modules.map((moduleJSON, index) => {
-								let module = JSON.parse(moduleJSON)
+	return (
+		<Container createTaskButton onClick={ clickHandler }>
+			<PlusIcon />
+		</Container>
+	)
+}
 
-								return React.createElement(
-									modules[module.type],
-									{
-										key: module.id,
-										meta: module,
-										full: false,
-										task
-									}
-								)
-							})
-						}
-					</Container>
-				)}
-			</Draggable>
-		)
+export default function Task({ taskID, index, listID }) {
+	if (taskID) {
+		return <RealTask taskID={ taskID } index={ index } />
 	} else {
-		return (
-			<Container createTaskButton onClick={ createTaskClickHandler }>
-				<PlusIcon />
-			</Container>
-		)
+		return <TaskPlaceholder listID={ listID } />
 	}
 }
